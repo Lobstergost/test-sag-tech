86b1449963269b7b73fb51b0e37d8d6e5871f829
Lobstergost <Lobstergost@gmail.com>
2024-04-29 01:52:34 +0300

    fix: appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import {PrismaService} from './prisma.service';
 import {AuthService} from './auth.service';
 import {UserService} from './user.service';
 import {PostService} from './post.service';
+import {AuthController} from './auth.controller';
+import {UserController} from './user.controller';
+import {PostController} from './post.controller';
 
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [AuthController, UserController, PostController],
   providers: [PrismaService, AuthService, UserService, PostService],
 })
 export class AppModule {}
